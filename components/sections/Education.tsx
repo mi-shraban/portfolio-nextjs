ac231e1d1e360d2d867634c6119d42718e4385a8
Md Monowarul Islam <monowarul7ii@gmail.com>
2025-10-21 19:15:26 +0600

    Font size updates. Also fixed pdf and all paths in a previous update.

--- components/sections/Education.tsx
+++ components/sections/Education.tsx
@@ -4,7 +4,7 @@ export default function Education() {
               <h2>Education</h2>
               <div className="card glow">
                   <h3><a className="ab_link" href="https://www.bracu.ac.bd/" target="_blank">Brac University</a></h3>
-                  <h5><b>Bachelor of Science in Computer Science</b></h5>
+                  <h4><b>Bachelor of Science in Computer Science</b></h4>
                   <ul>
                       <li>2021 - 2025</li>
                       <li>CGPA: 3.48 out of 4</li>
@@ -12,12 +12,12 @@ export default function Education() {
               </div>
               <div className="card">
                   <h3><a className="ab_link" href="https://www.rajukcollege.edu.bd/" target="_blank">Rajuk Uttara Model College</a></h3>
-                  <h5><b>Higher Secondary Certificate</b></h5>
+                  <h4><b>Higher Secondary Certificate</b></h4>
                   <ul>
                       <li>2018 - 2020</li>
                       <li>GPA: 5.00 out of 5</li>
                   </ul>
-                  <h5><b>Secondary School Certificate</b></h5>
+                  <h4><b>Secondary School Certificate</b></h4>
                   <ul>
                       <li>2013 - 2018</li>
                       <li>GPA: 5.00 out of 5</li>
--- components/sections/Skills.tsx
+++ components/sections/Skills.tsx
@@ -3,32 +3,32 @@ export default function Skills() {
 		<section id="skills" className="section">
 			<h2>Skills</h2>
 			<div className="card">
-				<h3>Programming Languages</h3>
+				<h4>Programming Languages</h4>
 				<ul>
 					<li>Python, C++, C</li>
 				</ul>
 			</div>
 			<div className="card">
-				<h3>Python Libraries &amp; Tools</h3>
+				<h4>Python Libraries &amp; Tools</h4>
 				<ul>
 					<li>NumPy, MatplotLib, Pandas, Scikit-Learn, TensorFlow, OpenCV, OpenGL</li>
 				</ul>
 			</div>
 			<div className="card">
-				<h3>Web Development</h3>
+				<h4>Web Development</h4>
 				<ul>
 					<li><b>Languages:</b> HTML, CSS, PHP, JavaScript</li>
 					<li><b>Frameworks:</b> Flask</li>
 				</ul>
 			</div>
 			<div className="card">
-				<h3>Databases</h3>
+				<h4>Databases</h4>
 				<ul>
 					<li>MySQL, SQLite</li>
 				</ul>
 			</div>
 			<div className="card">
-				<h3>Miscellaneous</h3>
+				<h4>Miscellaneous</h4>
 				<ul>
 					<li>LaTeX, Google Workspace, Microsoft Office</li>
 				</ul>
