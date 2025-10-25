963ffbc456e055e8a42b17b9c6991edde3fc53c9
Md Monowarul Islam <monowarul7ii@gmail.com>
2025-10-25 12:29:38 +0600

    metadata update in CF solves

---
+++ app/cfsolves/metadata.ts
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Md. Monowarul Islam Shraban - Codeforces Solves",
+	description: "Monowarul Islam CF solutions",
+	openGraph: {
+		title: "Monowarul Islam Shraban",
+		description: "Monowarul Islam CF solutions",
+		url: "https://monowarulislam.vercel.app/cfsolves",
+		siteName: "Monowarul Islam Shraban",
+		type: "website",
+	},
+};
