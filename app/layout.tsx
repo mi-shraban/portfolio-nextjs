485be8d5303bfafa82e82210d55acfaf678691c9
Md Monowarul Islam <monowarul7ii@gmail.com>
2025-11-03 17:22:00 +0600

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Space_Grotesk } from 'next/font/google'
 import Sidebar from "@/components/Sidebar";
+import { Analytics } from "@vercel/analytics/next"
 
 const bodyFont = Space_Grotesk({
     subsets: ['latin'],
